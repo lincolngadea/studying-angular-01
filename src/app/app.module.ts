aeb10dbe455415fe76698a719cacfe3ff60106fd
Lincoln Gadea <lincoln@glinc.com.br>
2020-11-28 18:03:22 -0300

    refactor: separetes components by order of responsibility

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,36 +5,24 @@ import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { CourseListComponent } from './courses/course-list.component';
 import { StarComponent } from './star/star.component';
 import { ReplacePipe } from './pipe/replace.pipe';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { Error404Component } from './error-404/error-4040.component';
-import { CourseInfoComponent } from './courses/course-info.component';
+import { CourseModule } from './courses/course-module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourseListComponent,
-    StarComponent,
-    ReplacePipe,
     NavBarComponent,
-    CourseInfoComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
+    CourseModule,
     RouterModule.forRoot([
-      {
-        path:'courses',
-        component: CourseListComponent
-      },
-      {
-        path:'courses/info/:id',
-        component:CourseInfoComponent
-      },
       {
         path:'',
         redirectTo:'courses',
